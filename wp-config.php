92be078199b54bc9f6f73bfc755b87b5b7c85231
Seb-Marquee <lopsebas@hotmail.fr>
2020-09-14 15:01:23 +0200

    update 14/09/2020 14h59

--- wp-config.php
+++ wp-config.php
@@ -13,20 +13,20 @@
  * * Database table prefix
  * * ABSPATH
  *
- * @link https://wordpress.org/support/article/editing-wp-config-php/
+ * @link https://codex.wordpress.org/Editing_wp-config.php
  *
  * @package WordPress
  */
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'DEFI-WEB' );
+define( 'DB_NAME', 'id14804176_wp_33852123c4f22e18d396610af2dc694c' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'id14804176_wp_33852123c4f22e18d396610af2dc694c' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'b7f48d8fa2a331be5791838d0106904ae9c1d0c3' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
@@ -37,14 +37,7 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-if ( !defined('WP_CLI') ) {
-    define( 'WP_SITEURL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
-    define( 'WP_HOME',    $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
-}
-
-
-
-/**#@+
+/**
  * Authentication Unique Keys and Salts.
  *
  * Change these to different unique phrases!
@@ -53,16 +46,15 @@ if ( !defined('WP_CLI') ) {
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'TqBROCoYCfhULlWfoW1axB9m8WOlxaUvjqgS13axnthYsLk2l1X1nzZpjFz7GhDK' );
-define( 'SECURE_AUTH_KEY',  'wAwctyeQjJiKAxbQjN0QPU1L1uuzcVrVSWiNKmfK3Jv547hYbAiD7imQNzWpr6Tx' );
-define( 'LOGGED_IN_KEY',    '8madX9gMfzlVZvHQne3khmtUoxpHUYXpOpAGrGXYDczkTbjY60eBL31BbdRDz4oz' );
-define( 'NONCE_KEY',        'CEN3gMM51qdj6OwY5rgHic0uhaY6grtIvrg2Wb6PdA4QuYJBb0e9FHzhPyWuCPKJ' );
-define( 'AUTH_SALT',        '8WqQyqhnMi7Hb8l8LFv5j3Q5aNxtCTZ2dsXmrGeHOZxn6M9fgaeGAm0XuT1mGC7T' );
-define( 'SECURE_AUTH_SALT', 'P3r0EBG9VKIgGYOCCJB2l1EDdnAU0LapNT5jILIppSFUBVFJV8LxSzx8RgpKxt0s' );
-define( 'LOGGED_IN_SALT',   'LXQWwiZnrO6ZylTtDDzzp5AcqU6pQ42ShIBSKpxX2hsr2ql391QJyiLLWE0FcnVp' );
-define( 'NONCE_SALT',       'R1Y6oevGe0qHiOatTAjXcws4t9uNOmeYPXa3LLl7EsPPxKpX40aiMRqbgdZxjTBv' );
-
-/**#@-*/
+define( 'AUTH_KEY',          'lZ#56Jf0nBuA w0!6tI78lhdB.Y~_l5J#u$B|5u}9w(A3}1!euV,5h+|a}_N%LK+' );
+define( 'SECURE_AUTH_KEY',   'sa,-C/6,3P@sr5Q[>.:!cE,>Fbxan,3D-X1Z8ne#<Fpp{l2{pZnXMuM`YzQ_|=EO' );
+define( 'LOGGED_IN_KEY',     '?iT)Qg|ul&EX4g[}mcbQQjW)mzTosTQs(evPtu+&hA?1Rx~8w8P#AIT#xnOJkROM' );
+define( 'NONCE_KEY',         '/C%3y${ ,)SmO~-&<;d|oSq9tS/Kj+b|1=[LbyJvl[Tu.W<On%sWH6G2a~M:cSFk' );
+define( 'AUTH_SALT',         'jMq*E41ss0~/|pVrEaP&?F2c` F[gX^+4>!B8:=KXJtP0#K>#>S6,WrlnKPa+r@-' );
+define( 'SECURE_AUTH_SALT',  'Qe=96B6zxfE[[p]%=ck;78%7K7rqGK/*wBgnB#;X<Z2^Zj$-TZ}$lM(16l5/6~C%' );
+define( 'LOGGED_IN_SALT',    '[[*d7Ob,bR=_<s%+ZZ`Z8a66l5|GT>mb+szvD[c6UqVL$_|Uyj WwzY1tvr~Zdc?' );
+define( 'NONCE_SALT',        'ugUaBOl|nBi2HD`;%)T)6sA1rt&(_-&]XbOILy[LE|eHw$B}?Pv<``)ecFA{%u!p' );
+define( 'WP_CACHE_KEY_SALT', 'lQogCX+,bYQTGyxjSd:R8C/v/Ex;T*i1X3`&T96!YN_Fpq3? 11jtJtS98k*Quek' );
 
 /**
  * WordPress Database Table prefix.
@@ -72,25 +64,14 @@ define( 'NONCE_SALT',       'R1Y6oevGe0qHiOatTAjXcws4t9uNOmeYPXa3LLl7EsPPxKpX40a
  */
 $table_prefix = 'wp_';
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- *
- * For information on other constants that can be used for debugging,
- * visit the documentation.
- *
- * @link https://wordpress.org/support/article/debugging-in-wordpress/
- */
-define( 'WP_DEBUG', false );
+
+
 
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
-	define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
 /** Sets up WordPress vars and included files. */
